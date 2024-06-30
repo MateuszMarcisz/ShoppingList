80df96e730224ce4190c862974c1a25ffc98e3f0
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-06-30 17:27:24 +0200

    accounts registration and login views

---
+++ Backend/Accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from Accounts.views import UserRegistrationAPIView, UserLoginAPIView, UserLogoutAPIView
+
+urlpatterns = [
+    path('register/', UserRegistrationAPIView.as_view(), name='user-register'),
+    path('login/', UserLoginAPIView.as_view(), name='user-login'),
+    path('logout/', UserLogoutAPIView.as_view(), name='user-logout'),
+]
---
+++ Backend/Accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import status, authentication, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from Accounts.serializers import UserSerializer
+
+
+class UserRegistrationAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+
+class UserLogoutAPIView(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
