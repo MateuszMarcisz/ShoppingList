0a823a32763db99891b29e238894d2878b786a93
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-07-01 15:29:12 +0200

    test ShoppingList vol.1

---
+++ Backend/ShoppingList/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from django.contrib.auth.models import User
+
+from ShoppingList.models import ShoppingList
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user(
+        username='test user',
+        password='test password',
+        email='test@example.com'
+    )
+
+
+@pytest.fixture
+def shopping_lists(user):
+    lst = []
+    for i in range(10):
+        lst.append(ShoppingList.objects.create(
+            name=f'list{i}',
+            user=user
+        ))
+    return lst
+
+
+@pytest.fixture
+def one_shopping_list(user):
+    ShoppingList.objects.create(
+            name='testing list',
+            user=user
+        )
+
--- Backend/ShoppingList/tests.py
+++ Backend/ShoppingList/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+from ShoppingList.models import ShoppingList
+
+
+@pytest.mark.django_db
+def test_shopping_list_get(shopping_lists, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('shoppinglist-list-create')
+    response = client.get(url)
+    assert response.status_code == 200
+    print(response.json())
+
+
+@pytest.mark.django_db
+def test_shopping_list_get_not_auth(shopping_lists):
+    client = Client()
+    url = reverse('shoppinglist-list-create')
+    response = client.get(url)
+    assert response.status_code == 401
+    print(response.json())
+    assert 'detail' in response.json()
+    assert response.json().get('detail') == 'Authentication credentials were not provided.'
+
+
+@pytest.mark.django_db
+def test_shopping_list_create(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('shoppinglist-list-create')
+    data = {
+        "name": "Create test",
+        "user": user.id
+    }
+    initial_count = ShoppingList.objects.count()
+    response = client.post(url, data)
+    assert response.status_code == 201
+    assert ShoppingList.objects.count() == initial_count + 1
+    print(response.json())
+
+
+@pytest.mark.django_db
+def test_shopping_list_create_not_auth(user):
+    client = Client()
+    url = reverse('shoppinglist-list-create')
+    data = {
+        "name": "Create test",
+        "user": user.id
+    }
+    initial_count = ShoppingList.objects.count()
+    response = client.post(url, data)
+    assert response.status_code == 401
+    assert ShoppingList.objects.count() == initial_count
+    assert 'detail' in response.json()
+    assert response.json().get('detail') == 'Authentication credentials were not provided.'
+
+
+@pytest.mark.django_db
+def test_shopping_list_create_empty_required_field_user(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('shoppinglist-list-create')
+    data = {
+        "name": "Create test",
+    }
+    initial_count = ShoppingList.objects.count()
+    response = client.post(url, data)
+    assert response.status_code == 400
+    assert ShoppingList.objects.count() == initial_count
+    assert "This field is required." in response.json()['user']
+    print(response.json())
+
+
+@pytest.mark.django_db
+def test_shopping_list_create_empty_required_field_user(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('shoppinglist-list-create')
+    data = {
+        "user": user.id
+    }
+    initial_count = ShoppingList.objects.count()
+    response = client.post(url, data)
+    assert response.status_code == 400
+    assert ShoppingList.objects.count() == initial_count
+    assert "This field is required." in response.json()['name']
+    print(response.json())
