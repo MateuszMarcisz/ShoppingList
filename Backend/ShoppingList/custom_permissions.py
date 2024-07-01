60a189463a02e84875a1cf8634b21251cabcbfb4
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-07-01 10:43:00 +0200

    new permission function

--- Backend/ShoppingList/custom_permissions.py
+++ Backend/ShoppingList/custom_permissions.py
@@ -1,11 +1,19 @@
 from rest_framework import permissions
 
+from ShoppingList.models import ShoppingList, Item
+
 
 # Permission to only allow owners of an object to view or edit it.
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # Object-level permission to only allow owners of the object to view/edit it.
-        return obj.user == request.user
+        # return obj.user == request.user
 
-
+        # If the object is a ShoppingList, check if the user is the owner.
+        if isinstance(obj, ShoppingList):
+            return obj.user == request.user
+        # If the object is an Item, check if the user is the owner of the related ShoppingList.
+        elif isinstance(obj, Item):
+            return obj.shopping_list.user == request.user
+        return False
