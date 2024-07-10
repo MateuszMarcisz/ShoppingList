8009d88c1ef6f500428d03e21ca0884be5d6880c
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-07-10 14:49:47 +0200

    add user already exists for backend to serialziation

--- Backend/Accounts/serializers.py
+++ Backend/Accounts/serializers.py
@@ -11,6 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
     # def create(self, validated_data):
     #     user = User.objects.create_user(**validated_data)
     #     return user
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with that username already exists.")
+        return value
 
     def create(self, validated_data):
         password = validated_data.pop('password')
